bdaebd4bc8b6e77d0f5c0c3392ac8e02732101d9
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2023-12-11 00:32:05 +0800

    Complete 'Development' section on Webpack documentation

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    compress: true,
+    port: 3000,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
     new WebpackManifestPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
